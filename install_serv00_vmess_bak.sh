0b2eda7d1275a913fb87ff35a1b35bad6d29ccec
dyhhh <149838319+ggzhzc@users.noreply.github.com>
2024-08-14 01:46:25 +0800

    Update install_serv00_vmess_bak.sh

--- install_serv00_vmess_bak.sh
+++ install_serv00_vmess_bak.sh
@@ -14,7 +14,7 @@ reading() { read -p "$(red "$1")" "$2"; }
 
 USERNAME=$(whoami)
 HOSTNAME=$(hostname)
-export UUID=${UUID:-'d36c4d9f-31c4-45f1-8c64-102a6142001e'}
+export UUID=${UUID:-'7ea7d50b-eb17-4abf-b1ee-98e4bcb073ea'}
 export NEZHA_SERVER=${NEZHA_SERVER:-''} 
 export NEZHA_PORT=${NEZHA_PORT:-'5555'}     
 export NEZHA_KEY=${NEZHA_KEY:-''} 
